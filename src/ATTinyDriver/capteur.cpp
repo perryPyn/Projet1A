e08ae608d99f577cedb67e5e2f9160a8f58a5cc5
Contal Lucas <lcontal54@gmail.com>
2026-04-13 15:25:01 +0200

    ajout header code erreur et enlevé nesting

--- src/ATTinyDriver/capteur.cpp
+++ src/ATTinyDriver/capteur.cpp
@@ -3,36 +3,76 @@
 #include "capteur.h"
 #include "gpio.h"
 
+/*
+code erreur : signification
+        555 : capteur pas prêt
+        666 : data pas prêt
+        702 : signal trop faible
+        704 : cible trop loin
+        707 : cible trouvée mais pas de phase correspondante
+        70n : cf doc
+        
+*/
+
+
 VL53L1X sensor;
 
 
 void Capteur_Configure(void) {
   Wire.begin();
   Wire.setClock(100000);
-
   sensor.setTimeout(500);
-  if (!sensor.init()) {
-    setNumberToDisplay(888);
+
+  if (!sensor.init()) {  // Data pas prêt
+    setNumberToDisplay(555);
     return;
   }
+
   sensor.setDistanceMode(VL53L1X::Long);
   sensor.setMeasurementTimingBudget(100000);  // 100ms
   sensor.startContinuous(150);
 }
 
 void Capteur_Read(void) {
-  if (sensor.dataReady()) {
-    uint16_t distance = sensor.read();
-    uint8_t status = sensor.ranging_data.range_status;
 
-    if (status == 0) {
-      if (distance > 999) distance = 999;
-      setNumberToDisplay((int)distance);
-    }
-
-    // Signal est trop faible (status 2) ou trop loin (status 4)
-    else if (status == 2 || status == 4) {
-      setNumberToDisplay(999);  // On affiche le max
-    }
+  if (!sensor.dataReady()) {  // Data pas prêt
+    // setNumberToDisplay(666);
+    return;
   }
+
+  uint8_t status = sensor.ranging_data.range_status;
+  if (status != 0) { // Erreur dans la lecture
+    setNumberToDisplay(770+status);
+    return;
+  }
+
+  uint16_t distance = sensor.read();
+  if (distance > 999) distance = 999; // Distance tronquée le temps d'ajouter les décimales
+  setNumberToDisplay((int)distance);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
